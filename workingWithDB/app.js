930e552b202c39fcc350237b3e06042169ecbd6e
DoQuocBinh <binhdq@fpt.edu.vn>
2021-10-09 15:59:47 +0700

    delete

--- workingWithDB/app.js
+++ workingWithDB/app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { insertToDB, getAll } = require('./databaseHandler')
+const { insertToDB, getAll, deleteObject } = require('./databaseHandler')
 const app = express()
 
 app.set('view engine','hbs')
@@ -9,7 +9,12 @@ app.get('/',async (req,res)=>{
     var result = await getAll("Products")
     res.render('home',{products:result})
 })
-
+app.get('/delete/:id',(req,res)=>{
+    const idValue = req.params.id
+    //viet ham xoa object dua tren id
+    deleteObject(idValue,"Products")
+    res.redirect('/')
+})
 app.post('/insert',(req,res)=>{
     const name = req.body.txtName
     const price = req.body.txtPrice
--- workingWithDB/databaseHandler.js
+++ workingWithDB/databaseHandler.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require('mongodb')
+const {MongoClient,ObjectId} = require('mongodb')
 
 const DATABASE_URL = 'mongodb://tommy:123456abc@cluster0-shard-00-00.lkrga.mongodb.net:27017,cluster0-shard-00-01.lkrga.mongodb.net:27017,cluster0-shard-00-02.lkrga.mongodb.net:27017/GCH0901_DB?replicaSet=Cluster0-shard-0&ssl=true&authSource=admin'
 const DATABASE_NAME = 'GCH0901_DB'
@@ -18,5 +18,11 @@ async function getAll(collectionName){
     return result
 }
 
-module.exports = {insertToDB,getAll}
+async function deleteObject(id,collectionName){
+    const client = await MongoClient.connect(DATABASE_URL);
+    const dbo = client.db(DATABASE_NAME);
+    dbo.collection(collectionName).deleteOne({_id:ObjectId(id)})
+}
+
+module.exports = {insertToDB,getAll,deleteObject}
 
