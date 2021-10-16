1757496ccaf50f5f5c0f5aa1c24be5ced5e10798
DoQuocBinh <binhdq@fpt.edu.vn>
2021-10-16 14:33:07 +0700

    aa

--- workingWithDB/databaseHandler.js
+++ workingWithDB/databaseHandler.js
@@ -4,36 +4,37 @@ const DATABASE_URL = 'mongodb://tommy:123456abc@cluster0-shard-00-00.lkrga.mongo
 const DATABASE_NAME = 'GCH0901_DB'
 
 async function updateDocument(id, updateValues,collectionName){
-    const client = await MongoClient.connect(DATABASE_URL);
-    const dbo = client.db(DATABASE_NAME);
+    const dbo = await getDatabase();
     await dbo.collection(collectionName).updateOne({_id:ObjectId(id)},updateValues)
 }
 
-async function getDocumentById(id,collectionName){
+async function getDatabase() {
     const client = await MongoClient.connect(DATABASE_URL);
     const dbo = client.db(DATABASE_NAME);
+    return dbo;
+}
+
+async function getDocumentById(id,collectionName){
+    const dbo = await getDatabase()
     const result = await dbo.collection(collectionName).findOne({_id:ObjectId(id)})
     return result;
 }
 
 //vi du obj la thong tin van insert, collection: ten cua bang can insert-vd Products
 async function insertToDB(obj,collectionName){
-    const client = await MongoClient.connect(DATABASE_URL);
-    const dbo = client.db(DATABASE_NAME);
+    const dbo = await getDatabase()
     const result = await dbo.collection(collectionName).insertOne(obj)
     console.log("Gia tri id moi duoc insert la: ", result.insertedId.toHexString());
 }
 
 async function getAll(collectionName){
-    const client = await MongoClient.connect(DATABASE_URL);
-    const dbo = client.db(DATABASE_NAME);
+    const dbo = await getDatabase()
     const result = await dbo.collection(collectionName).find({}).toArray()
     return result
 }
 
 async function deleteObject(id,collectionName){
-    const client = await MongoClient.connect(DATABASE_URL);
-    const dbo = client.db(DATABASE_NAME);
+    const dbo = await getDatabase()
     dbo.collection(collectionName).deleteOne({_id:ObjectId(id)})
 }
 
